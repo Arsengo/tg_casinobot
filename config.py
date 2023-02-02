46cbf20bac5eaf5b122cf151279b34bce36ec78f
Arsengo <124310641+Arsengo@users.noreply.github.com>
2023-02-03 03:32:36 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Весь код бота находится в main.py
-token = "TOKEN" # Токен бота
-botname = "CasinoBot" # Имя бота
-bmoney = 10000 # Сколько будет выдано игроку при старте
+token = "TOKEN" # Токен бота 5945333872:AAFd58c7rDkpaDur_A2xJs_oIAyFs-0q12E
+botname = "CasinoBot" # Имя бота Forex cc
+bmoney = 10000 # Сколько будет выдано игроку при старте 5000
 ownerid = 1279222472 # Тут chat id владельца бота чтобы его узнать его напишите боту /userid
+@Arsen9873
